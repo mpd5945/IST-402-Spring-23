fc1ca8ac680652521f2989e94bae76e9783733a1
PtirthP <106626340+PtirthP@users.noreply.github.com>
2023-03-25 23:46:07 -0400

    Update Python Caesar Brute Force LM1 Max Duverneuil.py

--- Python Caesar Brute Force LM1 Max Duverneuil.py
+++ Python Caesar Brute Force LM1 Max Duverneuil.py
@@ -1,45 +1,21 @@
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+import string
 
-def cipher(n, plaintextview):
-    #The following line will encrypt the string input by a user and return the ciphertext.
-    result = ''
-    for l in plaintextview:
-        try:
-            if l.isupper():
-                index = ALPHABET.index(l)
-                i = (index + n) % 26
-                result += ALPHABET[i]
-            else:
-                index = alphabet.index(l)
-                i = (index + n) % 26
-                result += alphabet[i]
-        except ValueError:
-            result += l
-    return result
+def caesar_decrypt(cipher_text, key):
+    #Decrypts the given cipher text using the given key
+    characters = string.ascii_letters + string.digits + string.punctuation + " "
+    plain_text = ""
+    for char in cipher_text:
+        index = characters.find(char)
+        index = (index - key) % len(characters)
+        plain_text += characters[index]
+    return plain_text
 
-def decipher(n, cipheredtext):
-    #The following line will decrypt the ciphered text from above and return a plain text legible response.
-    result = ''
-    for l in cipheredtext:
-        try:
-            if l.isupper():
-                index = ALPHABET.index(l)
-                i = (index - n) % 26
-                result += ALPHABET[i]
-            else:
-                index = alphabet.index(l)
-                i = (index - n) % 26
-                result += alphabet[i]
-        except ValueError:
-            result += l
-    return result
+def caesar_brute_force(cipher_text):
+    #Brute forces all possible keys to decrypt the given cipher text
+    for key in range(len(string.printable)):
+        plain_text = caesar_decrypt(cipher_text, key)
+        print("Key: {} | Decrypted text: {}".format(key, plain_text))
 
-
-message = "IST Four Oh Two With Joe Oakes Is Alright."
-key = 22
-enc = cipher(key, message)
-print("%d . %s" % (key, enc))
-
-dec = decipher(key, enc)
-print("%d . %s" % (key, dec))
+# Example usage:
+encrypted = 'Khoor Zruog $'
+caesar_brute_force(encrypted)
