ac8b916a6958d0e0d58170209ede9642ae5e25d8
PtirthP <106626340+PtirthP@users.noreply.github.com>
2023-03-26 01:05:55 -0400

    Update GoLang Caesar Brute Force.go

--- GoLang Caesar Brute Force.go
+++ GoLang Caesar Brute Force.go
@@ -5,52 +5,27 @@ import (
 	"strings"
 )
 
-func cipher(n int, plaintext string) string {
-	ALPHABET := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	alphabet := "abcdefghijklmnopqrstuvwxyz"
-	var result strings.Builder
-	for _, l := range plaintext {
-		if strings.ContainsRune(ALPHABET, l) {
-			index := strings.IndexRune(ALPHABET, l)
-			i := (index + n) % 26
-			result.WriteString(ALPHABET[i : i+1])
-		} else if strings.ContainsRune(alphabet, l) {
-			index := strings.IndexRune(alphabet, l)
-			i := (index + n) % 26
-			result.WriteString(alphabet[i : i+1])
-		} else {
-			result.WriteString(string(l))
-		}
+func shiftChar(ch rune, shift int) rune {
+	if ch >= 'a' && ch <= 'z' {
+		return 'a' + ((ch - 'a' - rune(shift) + 26) % 26)
+	} else if ch >= 'A' && ch <= 'Z' {
+		return 'A' + ((ch - 'A' - rune(shift) + 26) % 26)
+	} else {
+		return ch
 	}
-	return result.String()
 }
 
-func decipher(n int, cipheredtext string) string {
-	ALPHABET := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	alphabet := "abcdefghijklmnopqrstuvwxyz"
-	var result strings.Builder
-	for _, l := range cipheredtext {
-		if strings.ContainsRune(ALPHABET, l) {
-			index := strings.IndexRune(ALPHABET, l)
-			i := (index - n + 26) % 26
-			result.WriteString(ALPHABET[i : i+1])
-		} else if strings.ContainsRune(alphabet, l) {
-			index := strings.IndexRune(alphabet, l)
-			i := (index - n + 26) % 26
-			result.WriteString(alphabet[i : i+1])
-		} else {
-			result.WriteString(string(l))
+func bruteForce(cipherText string) {
+	for i := 1; i <= 99; i++ { // we try all possible shift values from 1 to 25
+		var plainText strings.Builder
+		for _, ch := range cipherText {
+			plainText.WriteRune(shiftChar(ch, i))
 		}
+		fmt.Printf("Key %d: %s\n", i, plainText.String())
 	}
-	return result.String()
 }
 
 func main() {
-	message := "Why did the Go programmer quit his job? He didn't get arrays."
-	key := 17
-	enc := cipher(key, message)
-	fmt.Printf("%d . %s\n", key, enc)
-
-	dec := decipher(key, enc)
-	fmt.Printf("%d . %s\n", key, dec)
+	cipherText := "Khoor Zruog!"
+	bruteForce(cipherText)
 }
